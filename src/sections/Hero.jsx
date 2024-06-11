a95c6e4bd3f44f84219b50263c31d785f68e52e4
Aditya Goyal <adityagoyaldoon@gmail.com>
2024-06-11 13:19:15 -0400

    About-us & Services section compete

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
 
   return (
-    <section id="home" className="w-full border-2 border-red-500 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-coral-red text-xl font-montserrat">Our Summer Collection</p>
         <h1 className="font-palanquin mt-10 text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold"><span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span><br /><span className="text-coral-red inline-block mt-3">Nike</span> Shoes</h1>
